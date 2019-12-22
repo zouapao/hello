9557014f1da57a22d44fa124772cf61bdc461760
chenhao <932206808@qq.com>
2019-12-22 23:05:38 +0800

    bug冷笑话

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("woenshi,里我都收了");
+        System.out.println("hello world");
+    }
+
+    public String retStr() {
+        return "becouse the light attract bugs!";
     }
 }
